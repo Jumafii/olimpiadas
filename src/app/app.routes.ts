1fa995af251bb3228656beff1a652eabf4ed14ad
Mateo Aires <mateoaires255@gmail.com>
2025-06-06 12:07:00 -0300

    Primer commit del proyecto Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
+import { registerAppScopedDispatcher } from '@angular/core/primitives/event-dispatch';
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
+import { Home } from './pages/home/home';
+import { Register } from './auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [Register,Login,Home];
