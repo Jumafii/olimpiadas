f24c439824f53f2bc9b71316fca44b807f51c224
JulianaTroncuto <juliana@gmail.com>
2025-06-13 13:32:26 -0300

    rama juli

--- src/app/auth/register/register.ts
+++ src/app/auth/register/register.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true, 
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.html',
-  styleUrl: './register.css'
+  styleUrls: ['./register.css'] 
 })
 export class Register {
+  nombre = '';
+  lastname = '';
+  email = '';
+  password = '';
+  confirmPassword = '';
+  errorPassword = false;
 
+  registrar() {
+    if (this.password !== this.confirmPassword) {
+      this.errorPassword = true;
+    } else {
+      this.errorPassword = false;
+      console.log('Registro exitoso:', {
+        nombre: this.nombre,
+        apellido: this.lastname,
+        email: this.email,
+        password: this.password
+      });
+    }
+  }
 }
+
